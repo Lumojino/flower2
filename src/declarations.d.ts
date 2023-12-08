25db92f00b77cc43e8266ceab525b9c053e9b162
Travis Johnson <lumojino@gmail.com>
2023-12-08 18:10:25 +0200

    djksdjkj

---
+++ src/declarations.d.ts
@@ -0,0 +1,11 @@
+declare module '*.mov' {
+    const src: string;
+    export default src;
+  }
+  
+  // declarations.d.ts
+declare module '*.webm' {
+    const src: string;
+    export default src;
+  }
+  
--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -5,9 +5,14 @@ import instagramIcon from '../../assets/svg/Instagram Old.svg';
 import tiktokIcon from '../../assets/svg/TikTok.svg';
 import xIcon from '../../assets/svg/TwitterX.svg';
 import facebookIcon from '../../assets/svg/Facebook.svg';
-import feeImage from '../../assets/svg/howitworkssvg.svg';
+import feeImageLarge from '../../assets/videos/Animation (1).webm';
+import feeImageSmall from '../../assets/svg/howitworkssvg.svg';
 
 const Home: React.FC = () => {
+  // Determine the resolution and choose the appropriate feeImage
+  const isLargeResolution = window.innerWidth > 450;
+  const FeeImageComponent = isLargeResolution ? FeeVideo : FeeImage;
+
   return (
     <div className={styles['home-container']}>
       <div className={styles['flex-container']}>
@@ -16,26 +21,35 @@ const Home: React.FC = () => {
           FOR EVERY OCCASION
         </h2>
         <div className={styles['social-icons-container']}>
-        {/* Individual social icons */}
-        <img src={instagramIcon} alt="Instagram" className={styles['social-icon']} />
-        <img src={tiktokIcon} alt="TikTok" className={styles['social-icon']} />
-        <img src={xIcon} alt="X" className={styles['social-icon']} />
-        <img src={facebookIcon} alt="Facebook" className={styles['social-icon']} />
-      </div>
+          {/* Individual social icons */}
+          <img src={instagramIcon} alt="Instagram" className={styles['social-icon']} />
+          <img src={tiktokIcon} alt="TikTok" className={styles['social-icon']} />
+          <img src={xIcon} alt="X" className={styles['social-icon']} />
+          <img src={facebookIcon} alt="Facebook" className={styles['social-icon']} />
+        </div>
       </div>
 
       {/* Add other content as needed */}
-
-    
 
       {/* How it Works section */}
       <div className={styles['how-it-works-container']}>
         <h3 className={styles['how-it-works-heading']}>HOW IT WORKS ?</h3>
         <button className={styles['how-it-works-button']}>SHOP NOW</button>
-        <img src={feeImage} alt="Fee" className={styles['feeImage']} />
+        <FeeImageComponent />
       </div>
     </div>
   );
 };
+
+const FeeImage: React.FC = () => (
+  <img src={feeImageSmall} alt="Fee" className={styles['feeImage']} />
+);
+
+const FeeVideo: React.FC = () => (
+  <video className={styles['feeVideo']} autoPlay loop muted>
+    <source src={feeImageLarge} type="video/webm" />
+    Your browser does not support the video tag.
+  </video>
+);
 
 export default Home;
